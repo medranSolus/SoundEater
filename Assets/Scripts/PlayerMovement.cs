6243d0ef54af70ae79df8894532b25adf11fa506
Kacper Antos <kacper.antos@codeteam.pl>
2020-03-17 20:59:57 +0100

    Dodanie dzwieku gracza oraz testowego zrodla dzwieku 3D na scenie

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,17 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     float moveSpeed = 5.0f;
+    [SerializeField]
+    AudioSource monsterFootstepSource;
 
     GameObject playerCamera;
 
+    Vector3 lastPosition;
+    
     void Start()
     {
         playerCamera = GameObject.Find("Player Camera");
+        lastPosition = transform.position;
     }
 
     void Update()
@@ -18,5 +23,11 @@ public class PlayerMovement : MonoBehaviour
         forward.y = 0.0f;
         forward.Normalize();
         transform.position += moveSpeed * Time.deltaTime * (Input.GetAxis("Vertical") * forward + Input.GetAxis("Horizontal") * (Quaternion.AngleAxis(90, Vector3.up) * forward));
+
+        if(monsterFootstepSource && (transform.position - lastPosition).magnitude > 3)
+        {
+            monsterFootstepSource.Play();
+            lastPosition = transform.position;
+        }
     }
 }
