bd779276f05e5aef3e266caf7ae07b432dc852fd
Radosław Stanulewicz <rad8broda@gmail.com>
2020-03-20 06:08:02 +0100

    Add audio source and footsteps sound

--- Assets/Scripts/Controllers/PreyController.cs
+++ Assets/Scripts/Controllers/PreyController.cs
@@ -8,10 +8,14 @@ public class PreyController : MonoBehaviour
     private NavMeshAgent _agent;
     public GameObject Player;
     public float EnemyDistanceRun = 4.0f;
+    [SerializeField]
+    AudioSource monsterFootstepSource;
+    Vector3 lastPosition;
 
     private void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
+        lastPosition = transform.position;
 
     }
 
@@ -20,11 +24,17 @@ public class PreyController : MonoBehaviour
         float distance = Vector3.Distance(transform.position, Player.transform.position);
         Debug.Log("Distance: " + distance);
 
+
         if(distance < EnemyDistanceRun)
         {
             Vector3 dirToPlayer = transform.position - Player.transform.position;
             Vector3 newPos = transform.position + dirToPlayer;
             _agent.SetDestination(newPos);
+        }
+        if (monsterFootstepSource && (transform.position - lastPosition).magnitude > 3)
+        {
+            monsterFootstepSource.Play();
+            lastPosition = transform.position;
         }
     }
     void OnDrawGizmosSelected()
