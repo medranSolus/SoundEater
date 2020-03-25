944bf6d36317821a71fce27a22900492107ca4c5
Kacper Antos <kacper.antos@codeteam.pl>
2020-03-25 14:51:14 +0100

    Added GameManager class

---
+++ Assets/GameManager/GameManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject PlayerPrefab;
+    // TO-DO
+    // Zmienic z GameObject na klase ofiary (punkty zycia, czy zjedzony itp.)
+    public GameObject PreyPrefab;
+
+    public int AmountOfPrey;
+    public float RoundTimeInSeconds;
+
+    public List<GameObject> SpawnPositions;
+
+    // Private variables to watch over game progress
+    private GameObject spawnedPlayer;
+    private List<GameObject> spawnedPrey;
+    private List<GameObject> leftSpawnPoints;
+    public float roundTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(SpawnPositions.Count < AmountOfPrey + 1 && SpawnPositions.Count > 1)
+        {
+            Debug.LogWarning("Not enough spawn points for every prey. Spawning less...");
+            AmountOfPrey = SpawnPositions.Count - 1;
+        }
+
+        if(SpawnPositions.Count == 1)
+        {
+            Debug.LogWarning("No spawn points left for prey.");
+            AmountOfPrey = 0;
+        }
+
+        if(SpawnPositions.Count == 0)
+        {
+            Debug.LogError("No spawn points!");
+            return;
+        }
+
+        leftSpawnPoints = SpawnPositions;
+
+        // Spawn player
+        int randomIndex = Random.Range(0, leftSpawnPoints.Count);
+        spawnedPlayer = Instantiate(PlayerPrefab, leftSpawnPoints[randomIndex].transform);
+        leftSpawnPoints.RemoveAt(randomIndex);
+
+        // Spawn prey
+        for(int i = 0; i < AmountOfPrey; i++)
+        {
+            randomIndex = Random.Range(0, leftSpawnPoints.Count);
+            spawnedPrey.Add(Instantiate(PreyPrefab, leftSpawnPoints[randomIndex].transform));
+            leftSpawnPoints.RemoveAt(randomIndex);
+        }
+
+        // Set variables
+        roundTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        roundTime += Time.deltaTime;
+
+        if(roundTime >= RoundTimeInSeconds)
+        {
+            Debug.Log("End of the round! We loose.");
+        }
+
+        // TO-DO
+        // Dodac kontrole zjedzonych ofiar
+    }
+}
