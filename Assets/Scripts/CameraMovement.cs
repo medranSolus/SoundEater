8ac53efdf1bd69f24b147303a33c4e8528ef5c5e
Marek Machliński <marekin450@spoko.pl>
2020-04-08 17:39:19 +0200

    Added game pause.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -10,18 +10,47 @@ public class CameraMovement : MonoBehaviour
     [SerializeField]
     [Range(275.0f, 355.0f)]
     float minLookUp = 280.0f;
+    [SerializeField]
+    KeyCode menuCode = KeyCode.Tab;
+    bool isGame = true;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
 
     void Update()
     {
-        transform.Rotate(new Vector3(
-            -Input.GetAxis("Mouse Y") * sensitivity,
-            Input.GetAxis("Mouse X") * sensitivity,
-            0.0f));
-        transform.rotation = Quaternion.Euler(
-            transform.rotation.eulerAngles.x <= 90.0f ?
-                Mathf.Clamp(transform.rotation.eulerAngles.x, 0.0f, maxLookDown) :
-                Mathf.Clamp(transform.rotation.eulerAngles.x, minLookUp, 360.0f),
-            transform.rotation.eulerAngles.y,
-            0.0f);
+        if (Input.GetKeyDown(menuCode))
+        {
+            if (isGame)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                Time.timeScale = 0.0f;
+                isGame = false;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                Time.timeScale = 1.0f;
+                isGame = true;
+            }
+        }
+        if (isGame)
+        {
+            transform.Rotate(new Vector3(
+                -Input.GetAxis("Mouse Y") * sensitivity,
+                Input.GetAxis("Mouse X") * sensitivity,
+                0.0f));
+            transform.rotation = Quaternion.Euler(
+                transform.rotation.eulerAngles.x <= 90.0f ?
+                    Mathf.Clamp(transform.rotation.eulerAngles.x, 0.0f, maxLookDown) :
+                    Mathf.Clamp(transform.rotation.eulerAngles.x, minLookUp, 360.0f),
+                transform.rotation.eulerAngles.y,
+                0.0f);
+        }
     }
 }
