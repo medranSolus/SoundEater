2923df37bfdaa029c5ff05dcf22002f19d9209d7
Radosław Stanulewicz <48476345+raito133@users.noreply.github.com>
2020-04-08 18:18:19 +0200

    019 user interface (#11)
    * Add UI
    
    Time text, crosshair, score text, dash indicator (placeholder) and enemies left count added, added catalog for fonts and images

---
+++ Assets/Scripts/HUD/TimeHUD.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeHUD : MonoBehaviour
+{
+    public GameManager gameManager;
+    public Text timeText;
+    private int time;
+    private int minutes, seconds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetText();
+    }
+    
+    // Split time into minutes and seconds
+    void SetText()
+    {
+        time = gameManager.getTimeLeft();
+        minutes = (time / 60);
+        seconds = time - minutes * 60;
+        timeText.text = minutes.ToString() + ":" + (seconds < 10 ? ("0" + seconds.ToString()) : seconds.ToString());
+    }
+}
