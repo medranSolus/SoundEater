2b00357f7b7106813dbb6390243153323e9d1909
Kacper Antos <kacper.antos@codeteam.pl>
2020-06-07 12:53:17 +0200

    Added volume slider functionality, options panel in pause menu, tweaked ending screen

--- Assets/Scenes/EndingScreenComponents/EndingScreen.cs
+++ Assets/Scenes/EndingScreenComponents/EndingScreen.cs
@@ -2,13 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class EndingScreen : MonoBehaviour
 {
+    public Text YourScore;
+
+    private void Start()
+    {
+        int score = PlayerPrefs.GetInt("LastScore");
+        YourScore.text = "Your score: " + score;
+    }
+
     public void PlayAgain()
     {
         Debug.Log("Changing scene to id = 2");
         SceneManager.LoadScene(1);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 
     public void QuitGame()
---
+++ Assets/Scripts/VolumeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioListener))]
+public class VolumeController : MonoBehaviour
+{
+    void Update()
+    {
+        AudioListener.volume = PlayerPrefs.GetFloat("MasterVolume", 0.5f);
+    }
+}
