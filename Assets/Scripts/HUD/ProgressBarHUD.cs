4052fc2b6f4af057c4b4df751c9027f07b132534
Radosław Stanulewicz <rad8broda@gmail.com>
2020-06-12 04:16:41 +0200

    Add progress bar object

---
+++ Assets/Scripts/HUD/ProgressBarHUD.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarHUD : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [MenuItem("GameObject/UI/Linear Progress Bar")]
+    public static void AddLinearProgressBar()
+    {
+        GameObject obj = Instantiate(Resources.Load<GameObject>("UI/Linear Progress Bar"));
+        obj.transform.SetParent(Selection.activeGameObject.transform, false);
+    }
+#endif
+    public int minimum;
+    public int maximum;
+    public int current;
+    public Image mask;
+    public Image fill;
+    public Color color;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetCurrentFill();
+    }
+
+    void GetCurrentFill()
+    {
+        float currentOffset = current - minimum;
+        float maximumOffset = maximum - minimum;
+        float fillAmount = (float)currentOffset / maximumOffset;
+        mask.fillAmount = fillAmount;
+        fill.color = color;
+    }
+}
